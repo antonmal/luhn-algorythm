8e352cf268bc48a5754d47ff38cc668d25629479
Anton Malkov <anton.malkov@gmail.com>
2015-11-28 15:24:38 +0100

    Initial commit

---
+++ luhn.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+class Luhn
+  attr_accessor :num
+
+  def initialize(num)
+    @num = num
+  end
+
+  def addends
+    digits = num.to_s.chars.map(&:to_i)
+    (2..digits.size).each do |i|
+      next if i.odd?
+      digits[-i] = digits[-i] * 2
+      digits[-i] -= 9 if digits[-i] > 9
+    end
+    digits
+  end
+
+  def checksum
+    addends.inject(:+)
+  end
+
+  def valid?
+    checksum % 10 == 0
+  end
+
+  def self.create(num_without_checksum)
+    last_digit = 10 - (Luhn.new(num_without_checksum * 10).checksum % 10)
+    last_digit = 0 if last_digit == 10
+    num_without_checksum * 10 + last_digit
+  end
+end
